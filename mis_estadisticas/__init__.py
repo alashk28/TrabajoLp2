b14b86b7a2574cef3ed32de35a47990537d607d9
alashk28 <arlxttx.alashka@gmail.com>
2025-10-27 09:06:30 -0500

    borrando un comentario

--- mis_estadisticas/__init__.py
+++ mis_estadisticas/__init__.py
@@ -1,4 +1,4 @@
 # Archivo: mi_estadistica/__init__.py
-from .base import EstadisticasBase # IMPORTANTE para la herencia
+from .base import EstadisticasBase
 from .cuantitativas import EstadisticasCuantitativas
 from .cualitativas import EstadisticasCualitativas
